0bb63397fd8752f0423728715fb71150c00129b9
zenyaboi <bannedboyinlife@gmail.com>
2022-03-16 18:43:43 -0300

    first commit

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.title = "Title"
+    t.version = "0.01"
+    t.console = true
+    t.window.widht = 1280
+    t.window.height = 720
+end
---
+++ player.lua
@@ -0,0 +1,45 @@
+Player = {}
+
+function Player:load()
+    self.x = 50
+    self.y = love.graphics.getHeight() / 2
+    self.width = 20
+    self.height = 100
+    self.speed = 500
+end
+
+function Player:update(dt)
+    self:move(dt)
+
+    self:checkBoundaries()
+end
+
+function Player:move(dt)
+    if love.keyboard.isDown("w") then
+        self.y = self.y - self.speed * dt
+    elseif love.keyboard.isDown("s") then
+        self.y = self.y + self.speed * dt
+    elseif love.keyboard.isDown("d") then
+        self.x = self.x + self.speed * dt
+    elseif love.keyboard.isDown("a") then
+        self.x = self.x - self.speed * dt
+    end
+end
+
+function Player:checkBoundaries()
+    if self.y < 0 then
+        self.y = 0
+    elseif self.y + self.height > love.graphics.getHeight() then
+        self.y = love.graphics.getHeight() - self.height
+    end
+
+    if self.x < 0 then
+        self.x = 0
+    elseif self.x + self.width > love.graphics.getWidth() then
+        self.x = love.graphics.getWidth() - self.width
+    end
+end
+
+function Player:draw()
+    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+end
